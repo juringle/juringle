38c6fa2752c34520d88d9ba4fe4fa190086242cb
juringle <greathans@gmail.com>
2026-04-27 02:51:33 +0900

    fix google oauth env vars

--- app.py
+++ app.py
@@ -56,8 +56,8 @@ app.secret_key = os.getenv("SECRET_KEY", "juringle-secret-2026")
 oauth = OAuth(app)
 google = oauth.register(
     name='google',
-    client_id=os.getenv("GOOGLE_CLIENT_ID"),
-    client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
+    client_id=os.environ.get("GOOGLE_CLIENT_ID", ""),
+    client_secret=os.environ.get("GOOGLE_CLIENT_SECRET", ""),
     server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
     client_kwargs={'scope': 'openid email profile'}
 )
